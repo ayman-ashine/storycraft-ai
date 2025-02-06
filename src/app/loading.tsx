971677ab6c1f7fb1b894b6c689487bbb4c1d9db0
ayman-ashine <gpx.zero.000@gmail.com>
2025-02-06 22:36:47 +0100

    fixing loading layout

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Screen, Spinner } from "@/components/ui"
 export default function Loading() {
     return (
         <Screen>
-            <Spinner size={40} spinnerColor="dark" bgColor="light" />
+            <Spinner size={40} />
         </Screen>
     )
 }
